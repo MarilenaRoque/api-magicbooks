2a5847ce72900d7b672f0fcb0cc9e97fe59ad071
MarilenaRoque <marilenas@id.uff.br>
2021-02-02 13:33:05 -0300

    Creating Routes and Controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,22 @@
 class BooksController < ApplicationController
+    def index
+        @book = Book.all
+        render json: @books
+    end
+
+    def create
+        @book = Book.create(
+            title: params[:title],
+            category: params[:category],
+            author: params[:author],
+        )
+        render json: @book
+    end
+
+    def destroy
+        @books = Book.all
+        @book = Book.find(params[:id])
+        @book.destroy
+        render json: @books
+    end
 end
